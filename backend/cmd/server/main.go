aef04d3cdfa37064c1f9566de07d515cb05841a7
Kleyton Solinho <kleytonsolinho@gmail.com>
2024-08-08 19:13:42 -0300

    feat: Add server main function with PostgreSQL database connection

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/kleytonsolinho/golang-websocket-react/backend/internal/api"
+	"github.com/kleytonsolinho/golang-websocket-react/backend/internal/store/pgstore/pgstore"
+)
+
 func main() {
-	// Call the function
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx,
+		fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+			os.Getenv("WS_DATABASE_USER"),
+			os.Getenv("WS_DATABASE_PASSWORD"),
+			os.Getenv("WS_DATABASE_HOST"),
+			os.Getenv("WS_DATABASE_PORT"),
+			os.Getenv("WS_DATABASE_NAME")),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to database")
+
+	handler := api.NewHandler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":8080", handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
 }
